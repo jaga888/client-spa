9d0f19e444c77782095a99582a98b7ee3d739a6b
Victoria <viktoryiacoyote@gmail.com>
2024-05-22 15:40:32 +0300

    task_1 | add validtion login

---
+++ components/LoginForm.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="card-body">
+    <form id="senex-login-form" action="/" method="post" novalidate
+          @submit.prevent="submitForm" ref="form">
+      <div class="form-group">
+        <label for="username">Email</label>
+        <input type="email"
+               v-model="user.email"
+               class="form-control"
+               id="form_user_user_name"
+               name="user_name"
+               placeholder="Enter email"
+               @input="v$.email.$touch"
+               :class="{ 'is-invalid': v$.email.$error }"
+               required/>
+        <div class="invalid-feedback" v-if="v$.email.$error">
+          {{ v$.email.$errors[0].$message }}
+          <!--          <span v-if="errorMessages.email">{{ errorMessages.email[0] }}</span>-->
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password"
+               v-model="user.password"
+               class="form-control"
+               id="form_user_password"
+               name="password"
+               placeholder="Password"
+               @input="v$.password.$touch"
+               :class="{ 'is-invalid': v$.password.$error }"
+               required
+        />
+
+      </div>
+      <div class="btn-toolbar justify-content-between">
+        <button type="submit" class="btn btn-primary">Login</button>
+        <a class="btn btn-link" href="/">Forgot my password</a>
+      </div>
+
+      <div class="invalid-feedback" v-if="v$.password.$error">
+        {{ v$.password.$errors[0].$message }}
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {required, email} from '@vuelidate/validators';
+import {useVuelidate} from "@vuelidate/core";
+import type {UserLogin} from "~/services/user/types";
+
+const form = ref();
+const {
+  signIn, token
+} = useAuth();
+
+const user = ref<UserLogin>({
+  email: "",
+  password: "",
+});
+
+const errorMessages = ref({});
+
+const rules = {
+  email: {required, email},
+  password: {required},
+};
+
+const v$ = useVuelidate(rules, user);
+
+const submitForm = async () => {
+  await v$.value.$validate();
+  if (!v$.value.$error) {
+    await signIn(user.value).catch(
+        (error: any) => {
+          errorMessages.value = error.response.data?.errors;
+        }
+    );
+  }
+};
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ pages/account/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container-fluid mt-5" style="overflow:auto">
+    <div class="row align-items-center justify-content-center">
+      <div class="col-md-4">
+        <div class="card border-0">
+          <div class="p-5 card-header bg-primary text-white" style="text-align: center;">
+            <img id="login_img" src="/media/images/logo/senex.svg" />
+          </div>
+          <LoginForm></LoginForm>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'login',
+  auth: {
+    unauthenticatedOnly: true,
+    navigateAuthenticatedTo: '/',
+  }
+})
+
+const { status } = useAuth()
+
+console.log('status l', status.value)
+</script>
